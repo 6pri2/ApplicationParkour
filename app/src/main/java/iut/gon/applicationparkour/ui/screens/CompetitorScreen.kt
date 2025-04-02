c03790bf4d654c775a643dd1ceae7d98938e47f0
GUERNINE Lyes <topsecret@gmail.com>
2025-04-02 15:43:33 +0200

    small fixes

--- app/src/main/java/iut/gon/applicationparkour/ui/screens/CompetitorScreen.kt
+++ app/src/main/java/iut/gon/applicationparkour/ui/screens/CompetitorScreen.kt
@@ -48,7 +48,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
+
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
